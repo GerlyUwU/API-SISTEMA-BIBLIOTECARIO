72442b1c8a59b2bdb921042243732e621f660dd9
Gerly Daniel Arteaga Bernal <arteaga.bernal3@gmail.com>
2024-05-29 03:09:00 -0600

    first commit

---
+++ src/main/java/com/uv/sistemaBibliotecario/SistemaBibliotecarioApplication.java
@@ -0,0 +1,13 @@
+package com.uv.sistemaBibliotecario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBibliotecarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaBibliotecarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uv/sistemaBibliotecario/SistemaBibliotecarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uv.sistemaBibliotecario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBibliotecarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
